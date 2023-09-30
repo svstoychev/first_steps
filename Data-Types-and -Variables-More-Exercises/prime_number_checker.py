d38ef42f2619480356ceb1ed84f7c1526ecd8492
svstoychev <oradieto@yahoo.com>
2023-09-30 15:43:43 +0300

    calculate prime number

---
+++ Data-Types-and -Variables-More-Exercises/prime_number_checker.py
@@ -0,0 +1,13 @@
+number = int(input())
+status = False
+if number == 1:
+    print(f'False')
+elif number > 1:
+    for i in range(2,number):
+        if number % i == 0:
+            status = True
+            break
+    if status:
+        print(f'False')
+    else:
+        print(f'True')
