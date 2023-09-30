4c9a2d11716638e7731357f409499285c523df83
svstoychev <oradieto@yahoo.com>
2023-09-30 16:16:04 +0300

    little decrypt message

---
+++ Data-Types-and -Variables-More-Exercises/decrypting_messages.py
@@ -0,0 +1,10 @@
+key = int(input())
+number_of_lines = int(input())
+decrypt = []
+result = 0
+for i in range (number_of_lines):
+    letter = input()
+    result = key + int(ord(str(letter)))
+    decrypt.append(chr(result))
+for word in decrypt:
+    print(f'{word}', end='')
